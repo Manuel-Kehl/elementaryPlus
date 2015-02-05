54aa60800d03f0cf3917fb7605cb14ca14ca3d0d
Stefan Ric <stfric369@gmail.com>
2015-02-06 00:17:28 +0100

    Remove unnecessary script and fix schema installer script

--- scripts/install_schema.sh
+++ scripts/install_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRCFILE="./scripts/apps.elementaryPlusInstaller.gschema.xml"
+SRCFILE="./scripts/postinst/apps.elementaryPlusInstaller.gschema.xml"
 DSTDIR="/usr/share/glib-2.0/schemas/"
 cp $SRCFILE $DSTDIR
 glib-compile-schemas $DSTDIR
--- scripts/sni-qt.sh
+++ scripts/sni-qt.sh
@@ -3,4 +3,3 @@
 add-apt-repository --yes ppa:cybre/sni-qt-eplus
 apt-get -y update
 apt-get -y install sni-qt sni-qt:i386
-add-apt-repository --remove --yes ppa:cybre/sni-qt-eplus
