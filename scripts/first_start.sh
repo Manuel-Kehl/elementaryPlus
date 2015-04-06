331a5f39035ea77fafb7911c7236348de0332401
Stefan Ric <stfric369@gmail.com>
2015-04-06 03:26:53 +0200

    Ooops

--- scripts/first_start.sh
+++ scripts/first_start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-pwd
-#pkexec policy
-SRCFILE="$1/scripts/postinst/apps.elementaryPlusInstaller.policy"
-DSTDIR="/usr/share/polkit-1/actions/"
-cp $SRCFILE $DSTDIR
-
 #glib2 schema
 SRCFILE="$1/scripts/postinst/apps.elementaryPlusInstaller.gschema.xml"
 DSTDIR="/usr/share/glib-2.0/schemas/"
