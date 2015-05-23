b6bae24392cc127fa03016339e23f9e6367d3733
Stefan Ric <stfric369@gmail.com>
2015-05-23 04:34:57 +0200

    Prepare installer for packaging

--- scripts/first_start.sh
+++ scripts/first_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#glib2 schema
-SRCFILE="$1/scripts/postinst/apps.elementaryPlusInstaller.gschema.xml"
+SRCFILE="$1/postinst/apps.elementaryPlusInstaller.gschema.xml"
 DSTDIR="/usr/share/glib-2.0/schemas/"
 cp $SRCFILE $DSTDIR
 glib-compile-schemas $DSTDIR
