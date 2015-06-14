6b1cc264151b5261026d324fcddfe1b02fa06424
Stefan Ric <stfric369@gmail.com>
2015-06-14 14:11:20 +0200

    New installer design

--- scripts/elementaryplus-installer.py
+++ scripts/elementaryplus-installer.py
@@ -8,7 +8,9 @@ import subprocess
 if not (Gtk.get_major_version() == 3 and Gtk.get_minor_version() >= 14):
     sys.exit("You need to have GTK 3.14 or newer to run this script")
 
-Notify.init("elementaryPlus Configurator")
+appName = "elementary+ Configurator"
+
+fromPPA = False
 
 scripts = os.getcwd() + "/scripts/"
 schema = "/usr/share/glib-2.0/schemas/apps.elementaryPlusConfigurator.gschema.xml"
@@ -16,24 +18,96 @@ schema = "/usr/share/glib-2.0/schemas/apps.elementaryPlusConfigurator.gschema.xm
 if os.path.isfile(schema) is False:
     subprocess.call(["pkexec", scripts + "first_start.sh", scripts])
 
-components = [["Core icon theme", "core"]]
-
-bins = [
-    ["flareGet", "/usr/bin/flareget"],
-    ["Google Music Manager", "/opt/google/musicmanager/google-musicmanager"],
-    ["MEGAsync", "/usr/bin/megasync"],
-    ["OwnCloud", "/usr/bin/owncloud"],
-    ["Seafile Client", "/usr/bin/seafile-applet"],
-    ["Skype", "/usr/bin/skype"],
-    ["Spotify", "/opt/spotify/spotify-client/spotify"],
-    ["Telegram Desktop", "%s/.TelegramDesktop/tdata/icon.png" % (os.getenv('HOME'))],
-    ["Tomahawk", "/usr/bin/tomahawk"],
-    ["WizNote", "/usr/bin/WizNote"]
+iconMegaList = [
+    [
+        "Core icon theme",
+        "",
+        "The core elementary+ icon theme",
+        "preferences-desktop"
+    ],
+    [
+        "flareGet",
+        "/usr/bin/flareget",
+        "FlareGet is a full featured, multi-threaded download manager and accelerator for Windows, Mac and Linux",
+        "flareget"
+    ],
+    [
+        "Google Music Manager",
+        "/opt/google/musicmanager/google-musicmanager",
+        "With Google Play Music for Chrome or Music Manager, you can add your personal music library to the cloud",
+        "google-musicmanager"
+    ],
+    [
+        "MEGAsync",
+        "/usr/bin/megasync",
+        "MEGAsync is a free online storage service",
+        "mega"
+    ],
+    [
+        "OwnCloud",
+        "/usr/bin/owncloud",
+        "An enterprise file sharing solution for online collaboration and storage",
+        "owncloud"
+    ],
+    [
+        "Seafile Client",
+        "/usr/bin/seafile-applet",
+        "The Seafile desktop client",
+        "seafile"
+    ],
+    [
+        "Skype",
+        "/usr/bin/skype",
+        "Stay in touch with your family and friends for free on Skype",
+        "skype"
+    ],
+    [
+        "Spotify",
+        "/opt/spotify/spotify-client/spotify",
+        "Spotify is a digital music service that gives you access to millions of songs",
+        "spotify-client"
+    ],
+    [
+        "Telegram Desktop",
+        "%s/.TelegramDesktop/tdata/icon.png" % (os.getenv('HOME')),
+        "Telegram is a messaging app with a focus on speed and security, it's super fast, simple and free",
+        "telegram"
+    ],
+    [
+        "Tomahawk",
+        "/usr/bin/tomahawk",
+        "A new kind of music player that invites all your streams, downloads, cloud music storage, playlists, radio stations and friends to the same party. It's about time they all mingle",
+        "tomahawk"
+    ],
+    [
+        "WizNote",
+        "/usr/bin/WizNote",
+        "Wiznote is a cloud based notes solution which helps personal and professional to take notes and collaborate with team members",
+        "wiznote"
+    ]
 ]
 
-for b in bins:
-    if os.path.isfile(b[1]):
-        components.append([b[0], b[0].lower().replace(" ", "_")])
+if fromPPA is True:
+    iconMegaList.reverse()
+    iconMegaList.pop()
+    iconMegaList.reverse()
+
+components = []
+
+iconTheme = Gtk.IconTheme
+defaultIconTheme = iconTheme.get_default()
+
+for a in iconMegaList:
+    name = a[0]
+    codeName = a[0].lower().replace(" ", "_")
+    shortDesc = (a[2][:60] + '..') if len(a[2]) > 60 else a[2]
+    icon = ("package-x-generic") if iconTheme.has_icon(defaultIconTheme, a[3]) == False else a[3]
+    enabled = (True) if os.path.isfile(a[1]) else False
+    if codeName == "core_icon_theme":
+        components.append([name, "core", shortDesc, icon, True])
+    else:
+        components.append([name, codeName, shortDesc, icon, enabled])
+
 
 toInstall = []
 toRemove = []
@@ -106,9 +180,9 @@ class useThemeDialog(Gtk.Dialog):
 class InstallerWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="elementary+ Configurator")
+        Gtk.Window.__init__(self, title=appName)
         self.set_border_width(10)
-        self.set_resizable(False)
+        self.set_size_request(500, 500)
         self.set_icon_name("preferences-desktop")
 
         self.hb = Gtk.HeaderBar()
@@ -124,25 +198,76 @@ class InstallerWindow(Gtk.Window):
 
         self.hb.pack_end(self.installButton)
 
-        lni = len(components)
-        self.table = Gtk.Table(lni, 3, True)
-        self.table.set_row_spacings(15)
-        self.table.set_col_spacings(10)
-        self.add(self.table)
+        Notify.init(appName)
+
+        self.add(self.build_ui())
+
+    def notify(self, messageOne, messageTwo, icon):
+        notification = Notify.Notification.new(messageOne, messageTwo, icon)
+        notification.show()
+
+    def build_ui(self):
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        availableIcons = self.create_icons_page()
+        vbox.pack_start(availableIcons, True, True, 0)
+
+        return vbox
+
+    def create_icons_page(self):
+        scroller = Gtk.ScrolledWindow(None, None)
+        scroller.set_shadow_type(Gtk.ShadowType.IN)
+        scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        lbox = Gtk.ListBox()
+        lbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        scroller.add(lbox)
 
         for i in range(len(components)):
+            longDesc = iconMegaList[i][2]
+            item = self.create_item(components[i][0], components[i][3], components[i][2], longDesc)
 
-            self.componentLabel = Gtk.Label(components[i][0], xalign=0)
-
-            self.componentSwitch = Gtk.Switch()
-            self.componentSwitch.props.halign = Gtk.Align.END
-            self.componentSwitch.connect("notify::active", self.callback, components[i][1])
+            componentSwitch = Gtk.Switch()
+            componentSwitch.props.halign = Gtk.Align.END
+            componentSwitch.props.valign = Gtk.Align.CENTER
+            componentSwitch.connect("notify::active", self.callback, components[i][1])
 
             if components[i][1] in installedComponents:
-                self.componentSwitch.set_active(True)
+                componentSwitch.set_active(True)
 
-            self.table.attach(self.componentLabel, 0, 2, i, i + 1)
-            self.table.attach(self.componentSwitch, 2, 3, i, i + 1)
+            wrap = Gtk.HBox(0)
+            wrap.pack_start(item, True, True, 0)
+            wrap.pack_end(componentSwitch, False, False, 2)
+
+            if components[i][4] is False:
+                wrap.set_sensitive(False)
+                wrap.set_tooltip_text("You must first install this app to select this!")
+            else:
+                wrap.set_tooltip_text(longDesc)
+
+            lbox.add(wrap)
+
+        return scroller
+
+    def create_item(self, name, iconName, shortDesc, longDesc):
+        grid = Gtk.Grid()
+        grid.set_border_width(16)
+        grid.set_row_spacing(4)
+        grid.set_column_spacing(16)
+
+        label = Gtk.Label("<big>%s</big>" % name)
+        label.set_use_markup(True)
+        label.set_alignment(0.0, 0.5)
+        icon = Gtk.Image.new_from_icon_name(iconName, Gtk.IconSize.DIALOG)
+        desc = Gtk.Label(shortDesc)
+        desc.get_style_context().add_class("dim-label")
+        desc.set_alignment(0.0, 0.5)
+        grid.attach(icon, 0, 0, 1, 2)
+        grid.attach(label, 1, 0, 1, 1)
+        grid.attach(desc, 1, 1, 1, 1)
+
+        return grid
+
+    def notsensitive():
+        notify("dsa", "dsa", "dasd")
 
     def callback(self, widget, event, data=None):
 
@@ -182,9 +307,7 @@ class InstallerWindow(Gtk.Window):
                         patchedSniqt = settings.get_boolean("sniqt-patched")
                         if data != "core" and data != "telegram_desktop" and patchedSniqt is False:
                             print "Installing patched sni-qt"
-                            notif = Notify.Notification.new('This may take a while', 'Please don\'t close the window', 'gnome-tweak-tool')
-                            if notif:
-                                notif.show()
+                            self.notify('This may take a while', 'Please don\'t close the window', 'gnome-tweak-tool')
                             if subprocess.call(['pkexec', scripts + "sni-qt.sh"]) == 0:
                                 settings.set_boolean("sniqt-patched", True)
 
@@ -222,20 +345,13 @@ class InstallerWindow(Gtk.Window):
                 toRemove[:] = []
                 toInstall[:] = []
 
-                notif = Notify.Notification.new('All changes applied', 'Check out your new icons!', 'gnome-tweak-tool')
-                if notif:
-                    notif.show()
+                self.notify('All changes applied', 'Check out your new icons!', 'gnome-tweak-tool')
 
                 self.installButton.set_sensitive(False)
 
             elif response == Gtk.ResponseType.CANCEL:
                 print("The Cancel button was clicked")
                 self.installButton.set_sensitive(True)
-
-        else:
-            notif = Notify.Notification.new('There is nothing to apply', 'You must change an option before applying!', 'gnome-tweak-tool')
-            if notif:
-                notif.show()
 
 win = InstallerWindow()
 win.connect("delete-event", Gtk.main_quit)
